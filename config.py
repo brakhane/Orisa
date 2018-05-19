7fcc487d057aad8320848ffc47a01f1d9caef023
Dennis Brakhane <brakhane@gmail.com>
2018-05-19 00:57:42 +0000

    comment config.py

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
-import logging
-
 #GUILD_ID = 42
 #CHANNEL_ID = 42
 
+# The guild this bot is a member of. It currently only supports
+# a single guild.
 GUILD_ID = 42
+
+# The channel congratulatory messages should be posted to
 CHANNEL_ID = 42
 
+# The ID of the owner, only this user is allowed
+# priviledged commands (mostly used for testing stuff)
 OWNER_ID = 42
+
+# Database URI for SQLAlchemy
 DATABASE_URI = 'sqlite:///database.sqlite'
 
+# The secret bot token
 BOT_TOKEN = ''
