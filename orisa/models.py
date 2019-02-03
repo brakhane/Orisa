dda7dd8032e64c10d8bf782775a6761415940910
Dennis Brakhane <brakhane@gmail.com>
2019-02-03 15:19:40 +0100

    Add and update copyright headers

--- orisa/models.py
+++ orisa/models.py
@@ -1,5 +1,5 @@
 # Orisa, a simple Discord bot with good intentions
-# Copyright (C) 2018 Dennis Brakhane
+# Copyright (C) 2018, 2019 Dennis Brakhane
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
