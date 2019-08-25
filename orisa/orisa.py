4fe24d0797ee7cae66aba96432ab31f4672cb27a
Dennis Brakhane <brakhane@gmail.com>
2019-08-25 12:30:14 +0200

    commit instead of flushing
    this might prevent multiple congrats

--- orisa/orisa.py
+++ orisa/orisa.py
@@ -2246,7 +2246,7 @@ class Orisa(Plugin):
 
             if rank is not None:
                 # get highest SR, but exclude current_sr
-                await run_sync(session.flush)
+                await run_sync(session.commit)
                 prev_highest_sr_value = session.query(func.max(type_to_check)).filter(
                     SR.handle == handle, SR.id != handle.current_sr_id
                 )
