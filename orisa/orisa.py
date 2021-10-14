86033c3a000d765f14702f4590c78cd3b04436e1
Dennis Brakhane <brakhane@gmail.com>
2021-10-15 01:06:06 +0200

    disable voice creating due to bug

--- orisa/orisa.py
+++ orisa/orisa.py
@@ -2173,6 +2173,8 @@ Retail Jedi"""
     async def _adjust_voice_channels(
         self, parent, *, create_all_channels=False, adjust_user_limits=False
     ):
+        logger.info("adjust disabled due to discord bug")
+        return
         logger.debug("adjusting parent %s", parent)
         guild = parent.guild
         if not guild:
@@ -2221,6 +2223,8 @@ Retail Jedi"""
         async def add_a_channel():
             nonlocal made_changes, chans, cat, guild
 
+            logger.info("Discord bug, not creating voice channels for now!")
+            return
             name = f"{prefix.strip()} #{len(chans)+1}"
             logger.debug("creating a new channel %s", name)
 
