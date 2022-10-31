2afc7ba556a5d72914aeba60a66d7c377736e0b4
Dennis Brakhane <brakhane@gmail.com>
2022-10-31 20:40:21 +0100

    first working version of career profile upload

--- submit/submit/models.py
+++ submit/submit/models.py
@@ -1,26 +1,9 @@
-# Orisa, a simple Discord bot with good intentions
-# Copyright (C) 2018, 2019 Dennis Brakhane
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, version 3 only
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import random
-import typing
-
 from contextlib import asynccontextmanager
 from datetime import datetime, timedelta
 from enum import Flag, auto
 
-import trio
-
+import sqlalchemy.types as types
 from sqlalchemy import (
     BigInteger,
     Boolean,
@@ -28,70 +11,33 @@ from sqlalchemy import (
     DateTime,
     ForeignKey,
     Integer,
+    LargeBinary,
     SmallInteger,
     String,
     create_engine,
-    func,
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.orderinglist import ordering_list
-from sqlalchemy.sql.functions import coalesce
 from sqlalchemy.orm import raiseload, relationship, sessionmaker
-import sqlalchemy.types as types
-
-from .config import DATABASE_URI
-from .utils import sr_to_rank, TDS, run_sync
-from .i18n import _, N_, NP_
+from sqlalchemy.sql.functions import coalesce
+from sqlalchemy.util import namedtuple
 
 Base = declarative_base()
 
+_ = NP_ = lambda x, y=None: x
 
-class Role(Flag):
-    NONE = 0
-    DPS = auto()
-    MAIN_TANK = auto()
-    OFF_TANK = auto()
-    SUPPORT = auto()
-
-    def format(self, ctx):
-        names = {
-            # Translators: Role
-            Role.DPS: _("Damage"),
-            # Translators: Role
-            Role.MAIN_TANK: _("Main Tank"),
-            # Translators: Role
-            Role.OFF_TANK: _("Off Tank"),
-            # Translators: Role
-            Role.SUPPORT: _("Support"),
-        }
-
-        return ", ".join(names[r] for r in Role if r and r in self)
+DATABASE_URI = "sqlite:///database-test.sqlite"
 
 
-class RoleType(types.TypeDecorator):
-    pytype = Role
-    impl = Integer
-
-    def process_bind_param(self, value, dialect):
-        return None if value is None else value.value
-
-    def process_result_value(self, value, dialect):
-        return None if value is None else self.pytype(value)
-
-    class comparator_factory(Integer.Comparator):
-        def contains(self, other, **kwargs):
-            return (self.op("&", return_type=types.Integer)(other)).bool_op("=")(
-                other.value
-            )
-
-
-class HighscoreCron(Base):
-    __tablename__ = "highscore_cron"
-
-    id = Column(BigInteger, primary_key=True, index=True)
-    last_run = Column(DateTime)
-    next_run = Column(DateTime, index=True)
+class ProfileUpload(Base):
+    __tablename__ = "profile_uploads"
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime, nullable=False)
+    interaction_token = Column(String, nullable=False)
+    user_id = Column(Integer, nullable=False)
+    image = Column(LargeBinary, nullable=False)
+    image_filename = Column(String, nullable=False)
 
 
 class User(Base):
@@ -110,11 +56,11 @@ class User(Base):
         lazy="joined",
         cascade="all, delete-orphan",
     )
-    last_problematic_nickname_warning = Column(DateTime)
+    # last_problematic_nickname_warning = Column(DateTime)
 
-    roles = Column(RoleType, nullable=False, default=Role.NONE)
+    # roles = Column(RoleType, nullable=False, default=Role.NONE)
 
-    always_show_sr = Column(Boolean, nullable=False, default=False)
+    # always_show_sr = Column(Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"<User(id={self.id}, discord_id={self.discord_id})>"
@@ -165,7 +111,7 @@ class Handle(Base):
     def last_update(self):
         return self.current_sr.timestamp if self.current_sr else None
 
-    def update_sr(self, new_srs, *, timestamp=None):
+    def update_sr(self, new_srs, *, hours_played=None, processed=None, timestamp=None):
         if timestamp is None:
             timestamp = datetime.utcnow()
 
@@ -185,6 +131,8 @@ class Handle(Base):
                 tank=new_srs.tank,
                 damage=new_srs.damage,
                 support=new_srs.support,
+                hours_played=hours_played,
+                processed=processed
             )
             self.sr_history.append(
                 sr_obj
@@ -298,6 +246,9 @@ class OnlineID(Handle):
         return f"<OnlineID(id={self.id} online_id={self.online_id})>"
 
 
+TDS = namedtuple("TDS", "tank damage support")
+
+
 class SR(Base):
     __tablename__ = "srs"
 
@@ -327,29 +278,8 @@ class SR(Base):
         self.damage = values.damage
         self.support = values.support
 
-    @property
-    def ranks(self):
-        return TDS(*[sr_to_rank(val) for val in self.values])
-
     def __repr__(self):
         return f"<SR(id={self.id}, values={self.values})>"
-
-
-class GuildConfigJson(Base):
-    __tablename__ = "guild_configs"
-
-    id = Column(BigInteger, primary_key=True, index=True)
-
-    config = Column(String, nullable=False)
-
-
-class WelcomeMessage(Base):
-    __tablename__ = "welcome_message"
-
-    id = Column(BigInteger, primary_key=True, index=True)
-    need_help_embed_id = Column(BigInteger)
-    is_private_message = Column(Boolean, nullable=False, default=False)
-    guild_name = Column(String)
 
 
 class Database:
@@ -361,75 +291,20 @@ class Database:
         self.Session = sessionmaker(bind=engine, autoflush=False)
         Base.metadata.create_all(engine)
 
-        self._min_delay = min(self._sync_delay(x) for x in range(10))
+    def user_by_id(self, session, id):
+        return session.query(User).filter_by(id=id).one_or_none()
 
-    @asynccontextmanager
-    async def session(self):
-        session = self.Session()
-        try:
-            yield session
-        finally:
-            await run_sync(session.close)
+    def handle_by_id(self, session, id):
+        return session.query(Handle).filter_by(id=id).one_or_none()
 
-    async def user_by_id(self, session, id):
-        return await run_sync(session.query(User).filter_by(id=id).one_or_none)
+    def user_by_discord_id(self, session, discord_id):
+        return session.query(User).filter_by(discord_id=discord_id).one_or_none()
 
-    async def handle_by_id(self, session, id):
-        return await run_sync(session.query(Handle).filter_by(id=id).one_or_none)
-
-    async def user_by_discord_id(self, session, discord_id):
-        return await run_sync(
-            session.query(User).filter_by(discord_id=discord_id).one_or_none
-        )
-
-    async def get_srs(self, session, discord_ids):
-        return await run_sync(
+    def get_srs(self, session, discord_ids):
+        return (
             session.query(SR)
             .join(Handle.current_sr, User)
             .filter(Handle.position == 0)
             .filter(User.discord_id.in_(discord_ids))
-            .all
+            .all()
         )
-
-    def _sync_delay(self, error_count):
-        if error_count == 0:
-            # slight randomization to avoid having all
-            # battletags update at the same time if Orisa didn't run
-            # for a while
-            return timedelta(minutes=random.randint(720, 722))
-        elif 0 < error_count < 3:
-            return timedelta(
-                minutes=5
-            )  # we actually want to try again fast, in case it was a temporary problem
-        elif 3 <= error_count < 5:
-            return timedelta(
-                minutes=840
-            )  # ok, the error's not going away, so wait longer
-        elif 5 <= error_count < 10:
-            # exponential backoff
-            return timedelta(minutes=840 + 20 * (error_count - 5) ** 2)
-        else:
-            return timedelta(days=1)
-
-    async def get_handles_to_be_synced(self, session):
-        results = await run_sync(
-            session.query(Handle)
-            .outerjoin(Handle.current_sr)
-            .filter(
-                coalesce(SR.timestamp, datetime.min)
-                <= datetime.utcnow() - self._min_delay
-            )
-            .all
-        )
-        return [
-            result.id
-            for result in results
-            if (result.last_update or datetime.min)
-            <= datetime.utcnow() - self._sync_delay(result.error_count)
-        ]
-
-    async def get_welcome_message(self, session, message_id):
-        msg = await run_sync(
-            session.query(WelcomeMessage).filter_by(id=message_id).one_or_none
-        )
-        return msg
---
+++ submit/submit/shared.py
@@ -0,0 +1,133 @@
+from enum import IntEnum
+from typing import Annotated, Any, Literal, Optional, Union
+
+from pydantic import BaseModel, Field, HttpUrl, Json
+from starlite.types import Message
+
+Snowflake = Union[int, str]
+
+
+class Interaction(BaseModel):
+    id: Snowflake
+    application_id: Snowflake
+    guild_id: Optional[Snowflake]
+    channel_id: Optional[Snowflake]
+    member: Optional["Member"]
+    user: Optional["User"]
+    token: str
+    version: Literal[1]
+    #    message: Message
+    app_permissions: Optional[str]
+    locale: Optional[str]
+    guild_locale: Optional[str]
+
+
+class Member(BaseModel):
+    user: "User"  # only missing in MESSAGE_CREATE/MESSAGE_UPDATE
+    nick: Optional[str]
+
+
+class User(BaseModel):
+    id: Snowflake
+    username: str
+    discriminator: str
+
+
+class InteractionType(IntEnum):
+    PING = 1
+    APPLICATION_COMMAND = 2
+    MESSAGE_COMPONENT = 3
+    APPLICATION_COMMAND_AUTOCOMPLETE = 4
+    MODAL_SUBMIT = 5
+
+
+class PingInteraction(Interaction):
+    type: Literal[InteractionType.PING]
+
+
+class CommandInteraction(Interaction):
+    type: Literal[InteractionType.APPLICATION_COMMAND]
+    data: "CommandInteractionData"
+
+
+class CommandInteractionData(BaseModel):
+    id: Snowflake
+    name: str
+    type: int
+    resolved: Optional["Resolved"]
+    options: Optional[list["CommandDataOption"]]
+    guild_id: Optional[Snowflake]
+    target_id: Optional[Snowflake]
+
+
+class MessageComponentInteraction(Interaction):
+    type: Literal[InteractionType.MESSAGE_COMPONENT]
+    data: "MessageComponentInteractionData"
+
+
+class MessageComponentInteractionData(BaseModel):
+    custom_id: str
+    component_type: "ComponentType"
+    values: Optional[list["SelectOption"]]
+
+
+class ComponentType(IntEnum):
+    ACTION_ROW = 1
+    BUTTON = 2
+    STRING_SELECT = 3
+    TEXT_INPUT = 4
+    USER_SELECT = 5
+    ROLE_SELECT = 6
+    MENTIONABLE_SELECT = 7
+    CHANNEL_SELECT = 8
+
+
+class SelectOption(BaseModel):
+    label: str
+    value: str
+    description: Optional[str]
+    # emoji
+    default: Optional[bool]
+
+
+class Resolved(BaseModel):
+    attachments: Optional[dict[Snowflake, "Attachment"]]
+
+
+class Attachment(BaseModel):
+    id: Snowflake
+    filename: str
+    description: Optional[str]
+    content_type: Optional[str]
+    size: int
+    url: HttpUrl
+    proxy_url: HttpUrl
+    height: Optional[int]
+    width: Optional[int]
+    ephemeral: Optional[bool]
+
+
+class CommandDataOption(BaseModel):
+    name: str
+    type: int
+    value: Optional[Union[str, int, float]]
+    # options
+    focused: Optional[bool]
+
+
+IncomingInteraction = Annotated[
+    Union[PingInteraction, CommandInteraction, MessageComponentInteraction],
+    Field(discriminator="type"),
+]
+
+Member.update_forward_refs()
+Interaction.update_forward_refs()
+PingInteraction.update_forward_refs()
+CommandInteraction.update_forward_refs()
+CommandInteractionData.update_forward_refs()
+Resolved.update_forward_refs()
+Attachment.update_forward_refs()
+MessageComponentInteraction.update_forward_refs()
+MessageComponentInteractionData.update_forward_refs()
+
+APPID = 445905377712930817
